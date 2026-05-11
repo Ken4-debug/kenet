b73d446a50a1a0504685d8307fff0d99635b3ad3
Kenet Kaljend <kenetkaljend@gmail.com>
2026-05-11 04:43:50 -0700

    Lisatud PowerShelli failid

--- TwoTask/Functions/Task01.ps1
+++ TwoTask/Functions/Task01.ps1
@@ -1,1 +1,22 @@
- dyukju
+# task_01.ps1
+# Skript valib juhusliku laiendi failist laiendid.txt ja lisab selle random.txt faili
+
+$file_name = "laiendid.txt"
+
+# Kontrollime, kas fail eksisteerib
+if (-Not (Test-Path $file_name)) {
+    Write-Output "Fail puudub!"
+    exit
+}
+
+# Loeme kõik laiendid faili
+$extensions = Get-Content $file_name
+
+# Valime juhusliku laiendi
+$random_index = Get-Random -Maximum $extensions.Count
+$random_extension = $extensions[$random_index]
+
+# Lisame random.txt faili
+Add-Content -Path "random.txt" -Value $random_extension
+
+Write-Output "Lisatud: $random_extension"
--- TwoTask/Functions/Task02.ps1
+++ TwoTask/Functions/Task02.ps1
@@ -1,1 +1,34 @@
-dyusr
+# task_02.ps1
+# Skript loeb random.txt faili ja ütleb, mitu korda kasutaja antud laiend failis esineb
+
+param (
+    [string]$extension
+)
+
+# Kontrollime, kas kasutaja andis laiendi argumendina
+if (-Not $extension) {
+    Write-Output "Palun sisesta otsitav laiend käsureal."
+    Write-Output "Näide: .\task_02.ps1 txt"
+    exit
+}
+
+$file_name = "random.txt"
+
+# Faili kontroll
+if (-Not (Test-Path $file_name)) {
+    Write-Output "Fail '$file_name' ei leidu. Käivita kõigepealt task_01.ps1."
+    exit
+}
+
+# Loeme faili read
+$lines = Get-Content $file_name
+
+# Arvutame, mitu korda laiend failis esineb
+$count = ($lines | Where-Object { $_ -eq $extension }).Count
+
+# Tulemuse kuvamine
+if ($count -gt 0) {
+    Write-Output "Laiend '$extension' esineb $count korda."
+} else {
+    Write-Output "Laiend '$extension' ei leitud."
+}
